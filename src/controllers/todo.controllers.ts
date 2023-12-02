13c149c3bb1f6e2980a94fa2e07a595a612ff660
Abhilash <abhilashjha335@gmail.com>
2023-12-02 19:52:33 +0530

    creating controllers for todos

--- src/controllers/todo.controllers.ts
+++ src/controllers/todo.controllers.ts
@@ -1,25 +1,53 @@
-import { BadRequest } from "../errors"
-import { Request, Response } from "express"
+import { BadRequest } from "../errors";
+import { Request, Response } from "express";
+import { todoServices } from "../services/todo.services";
+import { StatusCodes } from "http-status-codes" 
 
-const addTodo = async (req: Request, res: Response) => {
-  try {
-     res.status(201).send(req.body)
-  } catch (error) {
-    console.log(error)
-    throw new BadRequest("Something went wrong", 400);
-  }
-}
 
-const getTodo =  async (req: Request, res: Response) => {
-    try{
-     res.status(200).send('Message is successfully fetched!!');
-    } catch(error){
-        console.log(error);
-        throw new BadRequest("Nothing to get.", 400);
+class todoController {
+  // add todo controller
+  addTodo = async (req: Request, res: Response) => {
+ 
+    const { title, body } = req.body;
+
+    if(!title || !body) {
+      throw new BadRequest("Title and Body must be provided.", 400);
     }
+
+    const todo = await todoServices.createTodo(req.body);
+    res.status(StatusCodes.CREATED).json({ todo, msg: "Todo has been created!" });
+
+  };
+
+  // get all todos
+  getTodos = async (req: Request, res: Response) => {
+ 
+    const todos = await todoServices.getTodos();
+    res.status(StatusCodes.OK).json({ todos, msg:"All Todos has been fetched!"});
+
+  };
+
+  // get a single todo
+  getSingleTodo = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const todo = await todoServices.getATodo(id);
+    res.status(StatusCodes.OK).json({ todo, msg: "Todo has been fetched"});
+  }
+
+  // update todo
+  updateTodo = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const todo = await todoServices.updateTodo(id, req.body);
+    res.status(StatusCodes.OK).json({ todo, msg: "Todo has been updated"});
+  }
+
+  // delete todo
+  deleteTodo = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const todo = await todoServices.deleteTodo(id);
+    res.status(StatusCodes.OK).json({ todo, msg: "Todo has been deleted"});
+  }
+
 }
 
-export {
-    addTodo,
-    getTodo
-}
+export const todoControllers = new todoController();
