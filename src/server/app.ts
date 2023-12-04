980bb8dea958dc3b3a1b9830375a27684158ff41
Abhilash <abhilashjha335@gmail.com>
2023-12-05 01:42:18 +0530

    add cors

--- src/server/app.ts
+++ src/server/app.ts
@@ -4,12 +4,14 @@ import express from "express";
 import connectDB from "../config/db.config";
 import todoRouter from '../routes/todo.routes'
 import errorHandler  from "../middlewares/errorHandler.middlewares";
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
